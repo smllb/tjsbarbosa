e2afcfa112830286118e26539cb2016553f7f096
smllb <samuel.moreira6@fatec.sp.gov.br>
2024-06-24 23:37:55 -0300

    feat: add tjsp reference file

---
+++ config/tjsp/tjsp.ts
@@ -0,0 +1,24 @@
+const TJSP = {
+    // STRUCTURE
+    BASE_URL: "https://esaj.tjsp.jus.br/",
+    CPOPG: "cpopg",
+    OPEN_DO: "open.do",
+    SEARCH_DO: "search.do",
+    // QUERIES
+    CB_PLACEHOLDER: "cbPesquisa",
+    DATA_QUERY_PLACEHOLDER: "dadosConsulta.valorConsulta",
+    FORUM_ID_PLACEHOLDER: "cdForo",
+    // QUERY VALUES
+    CB_VALUE: {
+      nomeParte: "NMPARTE",
+      documentoParte: "DOCPARTE",
+      numeroProcesso: "NUMPROC",
+      nomeAdvogado: "NMADVOGADO",
+      numeroOab: "NUMOAB",
+      numeroPrecatoria: "PRECATORIA",
+      documentoDelegacia: "DOCDELEG",
+      numeroCDA: "NUMCDA"
+    }
+  };
+  
+  export default TJSP;
