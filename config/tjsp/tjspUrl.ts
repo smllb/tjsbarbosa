214060b5796d57c7bc40e5b8b53022b4cabc011c
smllb <samuel.moreira6@fatec.sp.gov.br>
2024-06-24 22:30:46 -0300

    feat: add base URL data

---
+++ config/tjsp/tjspUrl.ts
@@ -0,0 +1,24 @@
+// STRUCTURE
+export const BASE_URL = "https://esaj.tjsp.jus.br/";
+
+// ROUTES
+export const CPOPG = "cpopg";
+export const OPEN_DO = "open.do"
+
+// QUERY PARAMS
+export const CB_PLACEHOLDER = "cbPesquisa";
+export const DATA_QUERY = "dadosConsulta.valorConsulta"
+export const FORUM_ID = "cdForo"
+
+// VALUES
+export const CB_VALUE = {
+    nomeParte: "NMPARTE",
+    documentoParte: "DOCPARTE",
+    numeroProcesso: "NUMPROC",
+    nomeAdvogado: "NMADVOGADO",
+    numeroOab: "NUMOAB",
+    numeroPrecatoria: "PRECATORIA",
+    documentoDelegacia: "DOCDELEG",
+    numeroCDA: "NUMCDA"
+};
+
